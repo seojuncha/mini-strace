e2e7310b06a7c640fc93198d249923bba118bae9
Cha Seojun <sjcha36@gmail.com>
2025-05-03 11:39:41 +0900

    add script

--- run-qemu.sh
+++ run-qemu.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-qemu-system-arm -M virt -m 256M -cpu cortex-a7 -kernel zImage -initrd initramfs.cpio -append "console=ttyAMA0 rdinit=/init" -nographic
+qemu_cpu=cortex-a7
+qemu_memory=256M
+qemu_append_options="console=ttyAMA0 rdinit=/init"
+
+busybox_dir=$HOME/9/busybox-1.36.1/_install
+kernel_dir=$HOME/9/linux/arch/arm/boot
+
+zimage_path=$kernel_dir/zImage
+initramfs_path=$busybox_dir/../initramfs.cpio
+
+# Compile process
+echo "Compile"
+arm-linux-gnueabihf-gcc -static -o mini-strace mini-strace.c || exit
+
+if [ ! -d $buxybox_dir ]; then
+  echo "Directory not found: $busybox_dir"
+  exit
+fi
+
+cp mini-strace $busybox_dir/bin
+
+echo "Entering busybox directory: $busybox_dir"
+cd $busybox_dir
+
+echo "Creating initramfs.cpio.."
+find . | cpio -o --format=newc > $initramfs_path
+if [ ! -f $initramfs_path ]; then
+  echo "Fail: [$initramfs_path]"
+fi
+
+qemu-system-arm -M virt \
+  -m $qemu_memory \
+  -cpu $qemu_cpu \
+  -kernel $zimage_path \
+  -initrd $initramfs_path \
+  -append "$qemu_append_options" \
+  -nographic
