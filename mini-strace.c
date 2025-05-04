3069de8b3ef6bef6ba7262929ff3c895e4845ee0
Cha Seojun <sjcha36@gmail.com>
2025-05-04 10:23:53 +0900

    parse arguments and syscall name

--- mini-strace.c
+++ mini-strace.c
@@ -8,14 +8,64 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+struct syscall_entry {
+  int num;
+  const char *name;
+  int argc;
+};
+
+static const struct syscall_entry syscall_table[] = {
+  {0, "restart_syscall", 0},
+  {1, "exit", 0},
+  {2, "fork", 0},
+  {3, "read", 0},
+  {4, "write", 3},
+  {5, "open", 0},
+  {6, "close", 0},
+  {8, "create", 0},
+  {9, "link", 0},
+  {10, "unlink", 0},
+  {11, "execve", 3},
+  {12, "chdir", 0},
+  {14, "mknod", 0},
+  {15, "chmod", 0},
+  {16, "lchown", 0},
+  {19, "lseek", 0},
+  {20, "getpid", 0},
+  {21, "mount", 0},
+  {23, "setuid", 0},
+  {24, "getuid", 0},
+  {26, "ptrace", 0},
+  {29, "pause", 0},
+
+  {45, "brk", 1},
+  {125, "mprotect", 3},
+  {191, "ugetrlimit", 2},
+  {248, "exit_group", 1},
+  {256, "set_tid_address", 1},
+  {338, "set_robust_list", 2},
+  {332, "readlinkat", 4},
+  {384, "getrandom", 3},
+  {398, "rseq", 4},
+
+  {983045, "set_tls", 1}   // 0x0f0000 + 5
+  // and so on...
+};
+
+const struct syscall_entry *get_syscall_entry(int num) {
+  unsigned int sz = sizeof(syscall_table) / sizeof(struct syscall_entry);
+  for (int i = 0; i < sz; i++) {
+    if (num == syscall_table[i].num) {
+      return &syscall_table[i];
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   int ret;
   int status;
   bool in_syscall = false;
   pid_t pid;
-
-  printf("start!\n");
 
   if (argc < 2) {
     fprintf(stderr, "not enought arguments\n");
@@ -33,53 +83,37 @@ int main(int argc, char *argv[]) {
 
   if (pid == 0) {
     pid = getpid();
-    printf("tracee: %d\n", pid);
     if (ptrace(PTRACE_TRACEME, 0L, 0L, 0L) < 0) {
       perror("ptrace_trackme");
       exit(1);
     }
-    /*
-      Important:
-      - While being traced, the tracee will stop each time a signal is delivered, even if the signal is being ignored
-    */
+    /* Important:
+      - While being traced, the tracee will stop each time a signal is delivered, even if the signal is being ignored */
     kill(pid, SIGSTOP);
     execv(argv[1], &argv[1]);
+    // If the process reached here, there is an error.
+    perror("execv");
     exit(1);
   } else {
-    /*
-      When delivering system call traps, set bit 7 in the signal number (i.e., deliver SIGTRAP|0x80).
-    */
+    /* When delivering system call traps, set bit 7 in the signal number (i.e., deliver SIGTRAP|0x80).  */
     do {
-      
-      // if (ptrace(PTRACE_SETOPTIONS, pid, 0L, PTRACE_O_TRACESYSGOOD) < 0) {
-      //   perror("error");
-      //   exit(1);
-      // }
-
       ret = waitpid(pid, &status, __WALL);
       if (ret < 0) {
         perror("waitpid");
         exit(1);
       }
-      // syscall-enter-stop
-      // syscall-exit-stop
-      // signal-delivery-stop
-
-      /*
-       Syscall-enter-stop and syscall-exit-stop are observed by the
+      /* Syscall-enter-stop and syscall-exit-stop are observed by the
        tracer as waitpid(2) returning with WIFSTOPPED(status) true, and
        WSTOPSIG(status) giving SIGTRAP.  If the PTRACE_O_TRACESYSGOOD
        option was set by the tracer, then WSTOPSIG(status) will give the
        value (SIGTRAP | 0x80).
 
        Syscall-stops can be distinguished from signal-delivery-stop with 
-       SIGTRAP by querying PTRACE_GETSIGINFO for the following cases:
-       */
+       SIGTRAP by querying PTRACE_GETSIGINFO for the following cases: */
       if (WIFSTOPPED(status)) {
         // printf("stopped: %d\n", WSTOPSIG(status));
         switch (WSTOPSIG(status)) {
           case SIGSTOP:
-            // printf("sigstop\n");
             if (ptrace(PTRACE_SETOPTIONS, pid, 0L, PTRACE_O_TRACESYSGOOD) < 0) {
               perror("setoption");
               exit(1);
@@ -87,32 +121,41 @@ int main(int argc, char *argv[]) {
             break;
           case SIGTRAP | 0x80:
             struct user_regs regs;
+            if (ptrace(PTRACE_GETREGS, pid, 0L, &regs) < 0) {
+              perror("ptrace_getregs");
+              exit(1);
+            }
+            unsigned long sys_num = regs.uregs[7];
+            const struct syscall_entry *entry = get_syscall_entry(sys_num);
+            const char *sys_name = entry->name;
             if (!in_syscall) {
-              printf("Syscall Entry\n");
-              if (ptrace(PTRACE_GETREGS, pid, 0L, &regs) != -1) {
-                printf("  r0=%ld\n", regs.uregs[0]);
-                printf("  r7=%ld\n", regs.uregs[7]);
-                // write syscall
-                if (regs.uregs[7] == 4) {
-                  printf("WRITE with %ld, %ld, %ld\n", regs.uregs[0], regs.uregs[1], regs.uregs[2]);
-                  printf("May be dummy?: %ld\n", regs.uregs[3]);
+              if (sys_name) {
+                printf("[%d] %s(", sys_num, sys_name);
+                for (int i = 0; i < entry->argc; i++) {
+                  printf("r%d=%ld  ",i, regs.uregs[i]);
                 }
+
+                // only for test PEEK_DATA now
+                if (sys_num == 11) {
+                  struct user user_data;
+                  ptrace(PTRACE_PEEKDATA, pid, regs.uregs[0], 0L);
+                }
+                printf(")\n");
+              } else {
+                printf("unkonwn syscall: %ld\n", sys_num);
               }
               in_syscall = true;
             } else {
-              printf("Syscall Exit\n");
-              if (ptrace(PTRACE_GETREGS, pid, 0L, &regs) != -1) {
-                printf("  r0=%ld\n", regs.uregs[0]);
-                printf("  r7=%ld\n", regs.uregs[7]);
-              }
+              unsigned long sys_ret = regs.uregs[0];
+              // printf("  ret: %ld\n", sys_ret);
               in_syscall = false;
             }
             break;
           default:
-            printf("what: %d\n", WSTOPSIG(status));
+            printf("unknown stop signal: %d\n", WSTOPSIG(status));
             break;
         }
-        // Restart
+        // Restart a stopped tracee.
         if (ptrace(PTRACE_SYSCALL, pid, 0L, 0L) < 0) {
           perror("ptrace_syscall");
           break;
@@ -120,6 +163,5 @@ int main(int argc, char *argv[]) {
       }
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
-  printf("Done\n");
   return 0;
 }
